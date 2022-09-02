11b28288a482fc17aeb746ed635e96c5059e81b0
alexaurus <a.cole.sw@gmail.com>
2022-09-01 21:31:23 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,68 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myNewApp';
+  paymentHandler: any = null;
+  stripeAPIKey: any = '*********Add your stripe token here***********';
+
+  constructor() {}
+
+  /*------------------------------------------
+  --------------------------------------------
+  ngOnInit() Function
+  --------------------------------------------
+  --------------------------------------------*/
+  ngOnInit() {
+    this.invokeStripe();
+  }
+
+  /*------------------------------------------
+  --------------------------------------------
+  makePayment() Function
+  --------------------------------------------
+  --------------------------------------------*/
+  makePayment(amount: any) {
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: this.stripeAPIKey,
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken);
+        alert('Stripe token generated!');
+      },
+    });
+    paymentHandler.open({
+      name: 'ItSolutionStuff.com',
+      description: '3 widgets',
+      amount: amount * 100,
+    });
+  }
+
+  /*------------------------------------------
+  --------------------------------------------
+  invokeStripe() Function
+  --------------------------------------------
+  --------------------------------------------*/
+  invokeStripe() {
+    if (!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement('script');
+
+      script.id = 'stripe-script';
+      script.type = 'text/javascript';
+      script.src = 'https://checkout.stripe.com/checkout.js';
+      script.onload = () => {
+        this.paymentHandler = (<any>window).StripeCheckout.configure({
+          key: this.stripeAPIKey,
+          locale: 'auto',
+          token: function (stripeToken: any) {
+            console.log(stripeToken);
+            alert('Payment has been successfull!');
+          },
+        });
+      };
+
+      window.document.body.appendChild(script);
+    }
+  }
 }
